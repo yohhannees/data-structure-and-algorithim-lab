94ade2499d54697f609da75e225d6c2d3892bb54
Yohannees <yohannesgetachewerieso@gmail.com>
2022-12-15 11:51:50 +0300

    oop lab

---
+++ c++/datastructurelab2.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+void insertion_sort(int *num, int arraysize)
+{
+
+    for (int i = 0; i < arraysize; i++)
+    {
+        for (int j = i + 1; j < 10; j++)
+        {
+            if (num[i] > num[j])
+            {
+                int temp = num[i];
+                num[i] = num[j];
+                num[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        cout << num[i] << endl;
+    };
+}
+
+int main()
+{
+    int num[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    cout << "enter the key";
+    int key;
+    cin >> key;
+    for (int i = 0; i < 8; i++)
+    {
+        if (num[i] == key)
+        {
+            cout << "found at" << num[i];
+            ;
+        }
+        4
+    }
+
+    int num[] = {1, 3, 2, 4, 7, 6, 5, 8, 10, 9};
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = i + 1; j < 10; j++)
+        {
+            if (num[i] > num[j])
+            {
+                int temp = num[i];
+                num[i] = num[j];
+                num[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        cout << num[i] << endl;
+    };
+
+    int num[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int bottom = 0;
+    int top = 9;
+    int middle;
+    middle = (bottom + top) / 2;
+    cout << "enter the key";
+    int key;
+    cin >> key;
+    while (bottom <= top)
+    {
+
+        if (num[middle] == key)
+        {
+            cout << "found at  " << middle << endl;
+            return middle;
+        }
+        else if (num[middle] < key)
+        {
+            bottom = middle + 1;
+            middle = (bottom + top) / 2;
+        }
+        else if (num[middle] > key)
+        {
+            top = middle - 1;
+            middle = (bottom + top) / 2;
+        }
+    }
+    cout << "not found";
+
+    int num[] = {1, 3, 2, 4, 7, 6, 5, 8, 10, 9};
+    insertion_sort(num);
+
+    return 0;
+}
--- c++/datastructurelab3.cpp
+++ c++/datastructurelab3.cpp
@@ -29,32 +29,77 @@ void bubble_sort(int *num, int arraysize)
             }
         }
 }
-void merge_sort(int num, int arraysize)
+void merge_sort(int *num, int p, int q, int r)
 {
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    int l[n1], m[n2];
+    for (int i = 0; i < n1; i++)
+    {
+        l[i] = num[p + 1];
+    }
 
- 
+    for (int j = 0; j < n2; j++)
+    {
+        m[j] = num[q + 1 + j];
+    }
 
+    int i, j, k;
 
+    i = 0;
+    j = 0;
+    k = p;
 
+    while (i < n1 && j < n2)
+    {
+        if (l[i] <= m[j])
+        {
+            num[k] = l[i];
+            i++;
+        }
+        else
+        {
+            num[k] = m[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        num[k] = l[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        num[k] = m[j];
+        j++;
+        k++;
+    }
 }
+
 int main()
 
 {
 
-    int num[] = {1, 3, 2, 4, 7, 6, 5, 8, 10, 9};
+    // int num[] = {1, 3, 2, 4, 7, 6, 5, 8, 10, 9};
 
-    insertion_sort(num, 10);
-    cout << "the answer for insertion_sort" << endl;
-    for (int i = 0; i < 10; i++)
-    {
-        cout << num[i] << endl;
-    }
+    // insertion_sort(num, 10);
+    // cout << "the answer for insertion_sort" << endl;
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     cout << num[i] << endl;
+    // }
+    // cout << "the answer for bubble_sort" << endl;
+    // bubble_sort(num, 10);
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     cout << num[i] << endl;
+    // }
     cout << "the answer for bubble_sort" << endl;
-    bubble_sort(num, 10);
-    for (int i = 0; i < 10; i++)
-    {
-        cout << num[i] << endl;
-    }
+    
 
     return 0;
 }
