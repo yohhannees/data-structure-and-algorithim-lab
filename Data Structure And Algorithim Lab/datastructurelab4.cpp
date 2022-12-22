de6e60a3815d218dd72b7ff133517908f81c76e9
Yohannees <yohannesgetachewerieso@gmail.com>
2022-12-22 11:40:19 +0300

    updated

--- Data Structure And Algorithim Lab/datastructurelab4.cpp
+++ Data Structure And Algorithim Lab/datastructurelab4.cpp
@@ -1,5 +1,152 @@
 #include <iostream>
 using namespace std;
+
+struct node
+{
+    char name[10];
+    node *nxt;
+};
+
+node p;
+
+node *start_ptr = NULL;
+node *current;
+
+void add_node_at_the_end()
+{
+
+    node *temp, *temp2;
+    temp = new node;
+    cout << "enter name" << endl;
+    cin >> temp->name;
+    temp->nxt = NULL;
+
+    if (start_ptr == NULL)
+    {
+        start_ptr = temp;
+        current = start_ptr;
+    }
+    else
+    {
+        temp2 = start_ptr;
+
+        while (temp2->nxt != NULL)
+        {
+            temp2 = temp2->nxt;
+        }
+        temp2->nxt = temp;
+    }
+}
+
+void display_list()
+{
+    node *temp;
+    temp = start_ptr;
+    cout << endl;
+    if (temp == NULL)
+        cout << "The list is empty!" << endl;
+    else
+    {
+        while (temp != NULL)
+        {
+
+            cout << "Name : " << temp->name << " ";
+            if (temp == current)
+                cout << " <-- Current node";
+            cout << endl;
+            temp = temp->nxt;
+        }
+        cout << "End of list!" << endl;
+    }
+}
+
+void delete_start_node()
+{
+    node *temp;
+    temp = start_ptr;
+    start_ptr = start_ptr->nxt;
+    delete temp;
+}
+
+void delete_end_node()
+{
+    node *temp1, *temp2;
+    if (start_ptr == NULL)
+        cout << "The list is empty!" << endl;
+    else
+    {
+        temp1 = start_ptr;
+        if (temp1->nxt == NULL)
+        {
+            delete temp1;
+            start_ptr = NULL;
+        }
+        else
+        {
+            while (temp1->nxt != NULL)
+            {
+                temp2 = temp1;
+                temp1 = temp1->nxt;
+            }
+            delete temp1;
+            temp2->nxt = NULL;
+        }
+    }
+}
+
+void move_current_on()
+{
+    if (current->nxt == NULL)
+        cout << "You are at the end of the list." << endl;
+    else
+        current = current->nxt;
+}
+
+void move_current_back()
+{
+    if (current == start_ptr)
+        cout << "You are at the start of the list" << endl;
+    else
+    {
+        node *previous; 
+        previous = start_ptr;
+
+        while (previous->nxt != current)
+        {
+            previous = previous->nxt;
+        }
+        current = previous;
+    }
+}
+void add_in_middle()
+{
+    node *trav, *temp, *head;
+    trav = start_ptr;
+    while (trav != NULL)
+    {
+        if (strcmp(trav->name, "Abraham") == 0)
+
+        {
+            temp = new node;
+            cout << "Please enter the name : ";
+            cin >> temp->name;
+            
+            temp->nxt = NULL;
+            break;
+        }
+        else
+        {
+
+            trav = trav->nxt;
+        }
+    }
+    if (trav != NULL) // searched node found
+    {
+        temp->nxt = trav->nxt;
+        trav->nxt = temp;
+    }
+}
+
 int main()
 {
     // deletion from the array
@@ -19,29 +166,30 @@ int main()
     // }
 
     // insertion in to the array
-    int num[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int index;
-    cout << "enter the number index " << endl;
-    cin >> index;
-    cout << "enter the number value  you want to insert" << endl;
-    int value;
-    int temp;
-    cin >> value;
+    // int num[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // int index;
+    // cout << "enter the number index " << endl;
+    // cin >> index;
+    // cout << "enter the number value  you want to insert" << endl;
+    // int value;
+    // int temp;
+    // cin >> value;
+    // num[index] = value;
+    // temp = num[index];
 
-    temp = num[index];
-    num[index] = value;
-    for (int i = 10; i > index - 1; i--)
-    {
+    // for (int i = 10; i > index - 1; i--)
+    // {
 
-        num[i + 1] = num[i];
-    }
+    //     num[i + 1] = num[i];
 
-    num[index + 1] = temp;
+    // }
+    //   num[index + 1] = temp;
+    // for (int i = 0; i < 11; i++)
+    // {
+    //     cout << num[i] << "  ";
+    // }
 
-    for (int i = 0; i < 11; i++)
-    {
-        cout << num[i] << "  ";
-    }
+    // linked list
 
     return 0;
 }
--- Object Oriented Programming Lab/First.java
+++ Object Oriented Programming Lab/First.java
@@ -1,3 +1,5 @@
+import java.text.Format;
+import java.util.Random;
 import java.util.Scanner;
 
 import javax.lang.model.element.Name;
@@ -5,19 +7,47 @@ import javax.lang.model.element.Name;
 import org.xml.sax.helpers.NamespaceSupport;
 
 class First {
+
+  public static int[] questions() {
+    Random rand = new Random();
+    int one = rand.nextInt(100);
+    int two = rand.nextInt(100);
+    int arr[] = { one, two };
+    return arr;
+  }
+
+  static int score = 0;
+
+  public static void user() {
+    Scanner scan = new Scanner(System.in);
+    for (int i = 0; i < 5; i++) {
+
+      int[] arr = questions();
+      int addition = arr[0] + arr[1];
+      System.out.printf("what is %d + %d ?", arr[0], arr[1]);
+      int answer = scan.nextInt();
+      if (answer == addition) {
+        System.out.println("Correct");
+        score++;
+      } else {
+        System.out.println("Incorrect");
+
+      }
+
+    }
+
+    System.out.printf("your score is %d \n\n ", score);
+
+    System.out.printf("if you want to take the questions again press 1\n\n");
+    int choice = scan.nextInt();
+    if (choice == 1) {
+      user();
+    }
+
+  }
+
   public static void main(String[] args) {
-    String str = "I am good student";
-    String rev = reverse(str);
-    System.out.println("Reversed sentence: " + rev);
+    user();
   }
 
-  public static String reverse(String s) {
-
-    int x = s.indexOf(" ");
-
-    if (x == -1)
-      return s;
-
-    return reverse(s.substring(x + 1)) + " " + s.substring(0, x);
-  }
 }
