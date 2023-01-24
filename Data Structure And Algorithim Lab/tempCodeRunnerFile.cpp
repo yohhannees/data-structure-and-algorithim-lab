4fca868a79cc6a40eceddf18fdd924a803020a81
Yohannees <yohannesgetachewerieso@gmail.com>
2023-01-24 11:44:46 +0300

    done

--- Data Structure And Algorithim Lab/tempCodeRunnerFile.cpp
+++ Data Structure And Algorithim Lab/tempCodeRunnerFile.cpp
@@ -1,17 +1,1 @@
-
-    cout << "enter the number index " << endl;
-    cin >> index;
-    cout << "enter the number value  you want to insert" << endl;
-    int value;
-    int temp;
-    cin >> value;
-
-    temp = num[index];
-    num[index] = value;
-    for (int i = 10; i > index - 1; i--)
-    {
-
-        num[i + 1] = num[i];
-    }
-
-    num[index + 1] = temp;
+}
